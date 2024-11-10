dd1ea5be4ab5f45c64db512b0aec442504cd8eed
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-11-10 16:15:54 +0700

    fix(project): Add sorting function to projects list

--- src/app/(main)/recent-projects.tsx
+++ src/app/(main)/recent-projects.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import ProjectItem from "@/components/project-item";
 
 export default function RecentProjects(): React.ReactElement {
-  const projects = allProjects.slice(0, 3);
+  const projects = allProjects
+    .sort(
+      (a, b) =>
+        new Date(b.development_start).getTime() -
+        new Date(a.development_start).getTime(),
+    )
+    .slice(0, 3);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
